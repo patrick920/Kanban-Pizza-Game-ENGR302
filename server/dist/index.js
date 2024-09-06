524e00a746f3d72bb363a3c6fdfba6ab7838fce4
millspatr1 <millspatr1@ecs.vuw.ac.nz>
2024-09-06 21:35:58 +1200

    Code is currently broken. I will try to add Parcel to automatically bundle things.

---
+++ server/dist/index.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import { Server } from "socket.io";
+import path from 'path'; //Node.js module.
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//If port is not defined in the env file, use 3500.
+const PORT = process.env.PORT || 3500;
+//Refer to server as app???
+const app = express();
+//Middleware???
+//Will have problem later.
+//dirname is not available in modules, so will need to apply the fix.
+app.use(express.static(path.join(__dirname, "public")));
+const expressServer = app.listen(PORT, () => {
+    console.log(`listening on port ${PORT}`);
+});
+//const httpServer = createServer()
+const io = new Server(expressServer, {
+    //Will talk about CORS.
+    cors: {
+        //If it equals production then we'll say false. Don't want anyone outside of domain to
+        //access it. 5500 is where LiveServer hosts our application at.
+        //An issue will come up with this.
+        //If backend were on a different doamin, then you would have to list it.
+        //localhost and 127.0.0.1 are the same thing, but CORS interprets them as different things.
+        //With express, will host the front-end application with Express on the server as well. So won't
+        //have to address this further here.
+        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500",
+            "http://localhost:5501", "http://127.0.0.1:5501"
+        ]
+        //Note: I added 5501 above in case Live Server starts it with 5501 instead of 5500.
+    }
+});
+//Connection then listen.
+//Will echo message that was sent.
+io.on('connection', socket => {
+    console.log(`User ${socket.id} connected`);
+    socket.on('message', data => {
+        console.log(data);
+        //emit sends the message to EVERYONE who is connected to the server.
+        io.emit('message', `${socket.id.substring(0, 5)}: ${data}`); //Send that message back.
+    });
+});
+//# sourceMappingURL=index.js.map
---
+++ server/dist/public/app.js
@@ -0,0 +1,31 @@
+import { io } from 'socket.io-client';
+//Don't have to import io I think.
+const socket = io('ws://localhost:3500');
+//Send message might have event e.
+function sendMessage(e) {
+    e.preventDefault(); //Submit form without reloading the page.
+    const input = document.querySelector('input');
+    if (input.value) {
+        socket.emit('message', input.value);
+        input.value = ""; //After we send it, want to erase what's already in the input.
+    }
+    input.focus(); //Put the focus back on the input.
+}
+//Listening for the submit event of the form. Then call "sendMessage" function.
+document.querySelector('form')
+    .addEventListener('submit', sendMessage);
+//Listen for messages. "on" is the message event. Will call this function when we get an event I think.
+socket.on("message", (data) => {
+    //li is list item.
+    const li = document.createElement('li');
+    li.textContent = data; //data is essentially the message from the server.
+    //I think this should add a list item containing the server's message.
+    document.querySelector('ul').appendChild(li);
+});
+/*
+* -----------------------------------------------------------------------------------------------------
+* Phaser Code:
+* -----------------------------------------------------------------------------------------------------
+* Everything above was socket code, this below is Phaser code.
+*/ 
+//# sourceMappingURL=app.js.map

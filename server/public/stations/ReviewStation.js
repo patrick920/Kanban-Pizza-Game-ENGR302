982f0e79b9a735e7f8b688095e3e41e820528951
bullermax <bullermax@ecs.vuw.ac.nz>
2024-09-21 14:30:49 +1200

    Extended class a little and added comments with further execution  plan

--- server/public/stations/ReviewStation.js
+++ server/public/stations/ReviewStation.js
@@ -1,51 +1,64 @@
 /*
-* Kanban Board Station.
-* Patrick started working on this code. It was copied and pasted from Gabbie's "PrepareStation.js" code and then modified.
+* Review Station.
+* Max started working on this code. It was copied and pasted from Gabbie's "PrepareStation.js" code and then modified.
 */
+
+/*
+* Plans for implementing review station functionality:
+* Need to call pizza's info from somewhere to display the image of the finished pizza
+* Add section for notes to be taken on pizza which will be saved somewhere.
+* Could add a ticket class which stores the pizza object, 
+* notes about it, and the stage the pizza is at.
+* Tickets would show up on the kanban board in the section their stage indicates? 
+* Review class will display the pizza, the instructions given to make the pizza 
+* and have two buttons to either remake the pizza or allow it to be served. 
+* Review can't be completed without some notes being taken in the review section. 
+* At the end of the day could display review notes to help demonstrate 
+* what sections went well or didn't.
+*/
+
 import Station from './Station.js';
 import Pizza from './Pizza.js'; // Import the Pizza class
 
 export default class ReviewStation extends Station {
+    
+    
     constructor() {
         super({ key: 'ReviewStation' });
     }
 
     preload() {
-        // // load tomato paste image
-        // this.load.image('tomatoPaste', 'stations/assets/tomato_paste.png');
+       
     }
 
     create() {
         this.createBackground();
 
         // Game logic here
-        this.add.text(100, 100, 'Review', { fontSize: '32px', fontFamily: 'Calibri', fill: '#fff' });
+        // Add review title to top of screen
+        this.add.text(100, 100, 'Review', { fontSize: '32px', fontFamily: 'Calibri', fill: '#fff'});
 
-        // Navigation buttons (from Station.js)
-        this.createNavigationTabs();
+        // Add button to go back to kanban board in bottom right
+        this.add.text(50, this.game.config.height - 100, 'Back to Kanban Board', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#996600' })
+            .setInteractive()
+            .on('pointerdown', () => this.scene.start('KanbanStation'));
 
-        // Create tomato paste image
-        //this.createTomatoPasteBottle();
+        //Add a text box on right side of screen people can type in
+
+        //Add button to restart order - needs functionality
+        this.add.text(200, this.game.config.height - 100, 'Reject', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#f44336' });
+
+        //Add button to allow order through - needs functionality
+        this.add.text(250, this.game.config.height - 100, 'Serve', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#8fce00' });
+
     }
 
     createBackground() {
-        // Set a specific background color for the PrepareStation
-        this.cameras.main.setBackgroundColor('#996600');
-        //this.createPizzaBaseButton(); // Setup pizza base buttons
+        // Set a specific background color for the ReviewStation
+        this.cameras.main.setBackgroundColor('#00e699');
+
+        // Add functionality here to display the pizza image 
     }
 
-    /*
-    createPizzaBaseButton() {
-        const baseSmallButton = this.add.text(50, 200, 'Small Pizza Base', { fontSize: '20px', fill: '#000', fontFamily: 'Calibri', backgroundColor: '#ffd11a' })
-            .setInteractive()
-            .on('pointerdown', () => {
-                this.createPizza('small');
-            });
-        const baseLargeButton = this.add.text(50, 250, 'Large Pizza Base', { fontSize: '20px', fill: '#000', fontFamily: 'Calibri', backgroundColor: '#ffd11a' })
-            .setInteractive()
-            .on('pointerdown', () => {
-                this.createPizza('large');
-            });
-    }
-    */
+
 }

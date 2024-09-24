7593b4682ac89b55360e8ed4f9a2e7b560290911
millspatr1 <millspatr1@myvuw.ac.nz>
2024-09-24 18:35:22 +1200

    Displayed the titles for the Kanban board columns.

--- server/public/stations/KanbanStation.js
+++ server/public/stations/KanbanStation.js
@@ -13,8 +13,8 @@ import { makeDraggable } from './kanban/draggable.js';
 //Source: https://www.w3schools.com/js/js_const.asp
 const TOP_TO_TITLE_GAP = 10;
 const TITLE_TEXT_HEIGHT = 40;
-const TITLE_TO_COLUMN_TITLES_HEIGHT = 10;
-const COLUMN_TITLES_TEXT_HEIGHT = 15;
+const TITLE_TO_COLUMN_TITLES_HEIGHT = 5; //Used to be 10.
+const COLUMN_TITLES_TEXT_HEIGHT = 20; //Used to be 15.
 //The width of the Kanban board column.
 const COLUMN_RECTANGLE_WIDTH = 200;
 //The gap between the Kanban board column and the menu at the bottom of the screen.
@@ -55,9 +55,9 @@ export default class KanbanStation extends Station {
         //Source: https://newdocs.phaser.io/docs/3.80.0/Phaser.Types.GameObjects.Text.TextStyle
         //Some code from ChatGPT:
         //650 as this is the middle.
-        const titleText = this.add.text(650, TOP_TO_TITLE_GAP, 'Kanban Board', { fontSize: '40px', fontFamily: 'Calibri', fill: '#fff' });
+        const titleText = this.add.text(650, TOP_TO_TITLE_GAP, 'Kanban Board', { fontSize: TITLE_TEXT_HEIGHT + 'px', fontFamily: 'Calibri', fill: '#fff' });
 
-         //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
         titleText.setOrigin(0.5, 0);
 
         // Navigation buttons (from Station.js)
@@ -152,12 +152,89 @@ export default class KanbanStation extends Station {
         completedColumnRectangle.setOrigin(0, 0);
         increaseCurrentXColumnRectangleStartPos(); //Increase the starting X position before drawing the next column rectangle.
 
-        // this.orderTickets = [];
-        // this.prepTickets = [];
-        // this.cookTickets = [];
-        // this.reviewTickets = [];
-        // this.serviceTickets = []; 
-        // this.completedTickets = []; 
+        //-----------------------------------------------------------
+        //Create the text labels for each of the columns.
+        //-----------------------------------------------------------
+
+        const Y_TOP_COLUMN_TITLES = TOP_TO_TITLE_GAP + TITLE_TEXT_HEIGHT + TITLE_TO_COLUMN_TITLES_HEIGHT
+
+        //This variable will be incremented as I draw each of the rectangles for the columns on the Kanban board.
+        //Set it at the initial position for the first rectangle.
+        let currentXColumnTitleStartPos = GAP_BETWEEN_COLUMN_RECTANGLES + ADDITIONAL_GAP_BESIDE_COLUMN_RECTANGLES_LEFT_RIGHT_SCREEN;
+        //Will also need to centre it, not in this variable but in the actual code to display the text on the screen.
+        let currentXColumnTitleCenterPos = 0;
+        updateCurrentXColumnTitleCenterPos(); //Set this initially for the first time.
+
+        /**
+         * Increment the position to start drawing each new column title in the Kanban board.
+         * Additionally call the function to update the variable that holds the centred position to draw the text.
+         */
+        function increaseCurrentXColumnTitleStartPos(){
+            currentXColumnTitleStartPos += GAP_BETWEEN_COLUMN_RECTANGLES + COLUMN_RECTANGLE_WIDTH;
+            //Also set the actual position to draw the text so that it is centred.
+            updateCurrentXColumnTitleCenterPos();
+        }
+
+        /**
+         * Set the actual position to draw the text so that it is centred.
+         */
+        function updateCurrentXColumnTitleCenterPos(){
+            currentXColumnTitleCenterPos = currentXColumnTitleStartPos + (COLUMN_RECTANGLE_WIDTH / 2);
+        }
+
+        console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        const orderText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Orders', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+            fontFamily: 'Calibri', fill: '#fff' });
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        orderText.setOrigin(0.5, 0);
+        //Update the two variables that store the X positions for drawing the text.
+        increaseCurrentXColumnTitleStartPos();
+
+        console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        const prepText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Preparation', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+            fontFamily: 'Calibri', fill: '#fff' });
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        prepText.setOrigin(0.5, 0);
+        //Update the two variables that store the X positions for drawing the text.
+        increaseCurrentXColumnTitleStartPos();
+
+        console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        const cookText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Cooking and Cutting', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+            fontFamily: 'Calibri', fill: '#fff' });
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        cookText.setOrigin(0.5, 0);
+        //Update the two variables that store the X positions for drawing the text.
+        increaseCurrentXColumnTitleStartPos();
+
+        console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        const reviewText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Reviewing', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+            fontFamily: 'Calibri', fill: '#fff' });
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        reviewText.setOrigin(0.5, 0);
+        //Update the two variables that store the X positions for drawing the text.
+        increaseCurrentXColumnTitleStartPos();
+
+        console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        const serviceText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Service', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+            fontFamily: 'Calibri', fill: '#fff' });
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        serviceText.setOrigin(0.5, 0);
+        //Update the two variables that store the X positions for drawing the text.
+        increaseCurrentXColumnTitleStartPos();
+
+        console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        const completedText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Completed Pizzas', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+            fontFamily: 'Calibri', fill: '#fff' });
+        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        completedText.setOrigin(0.5, 0);
+        //Update the two variables that store the X positions for drawing the text.
+        increaseCurrentXColumnTitleStartPos();
 
         //cureentXStartPos += ___ + ___;
 
@@ -167,6 +244,7 @@ export default class KanbanStation extends Station {
         //-----------------------------------------------------------
         //Test object only.
         //-----------------------------------------------------------
+
         //Add rectangle to the screen. This rectangle can be dragged around if you hold click and move your mouse.
         //The code to update the rectangle's position when it is dragged is in "draggable.js".
         //Code below from: https://www.youtube.com/watch?v=jWglIBp4usY&ab_channel=ScottWestover

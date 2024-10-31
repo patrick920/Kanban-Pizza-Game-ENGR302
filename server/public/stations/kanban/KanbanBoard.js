daaa7092ef458ed1632ca488501c2486b2b53821
millspatr1 <millspatr1@ecs.vuw.ac.nz>
2024-11-01 12:10:52 +1300

    I fixed the issues after moving the code to display the Kanban board to KanbanBoard.js, and it now displays successfully.

--- server/public/stations/kanban/KanbanBoard.js
+++ server/public/stations/kanban/KanbanBoard.js
@@ -1,9 +1,26 @@
 /*
- * This file will hold all the display code for the Kanban board, to keep KanbanStation.js cleaner and not too long.
+ * kanbanStation file will hold all the display code for the Kanban board, to keep KanbanStation.js cleaner and not too long.
  */
 
 //TODO: Have a list for each of the stations with the labels currently in there. Have a hardcoded list for each
-//one as it will be simpler.
+//one as it will be simpler. NO, get the list from the KanbanStation that has already been defined.
+
+//Source: https://www.w3schools.com/js/js_const.asp
+const TOP_TO_TITLE_GAP = 10;
+const TITLE_TEXT_HEIGHT = 40;
+const TITLE_TO_COLUMN_TITLES_HEIGHT = 5; //Used to be 10.
+const COLUMN_TITLES_TEXT_HEIGHT = 20; //Used to be 15.
+//The width of the Kanban board column.
+const COLUMN_RECTANGLE_WIDTH = 200;
+//The gap between the Kanban board column and the menu at the bottom of the screen.
+const COLUMN_RECTANGLES_TO_MENU_GAP = 10;
+//Y position for the start of the buttons for the menu at the bottom of the screen.
+const Y_MENU_START = 100;
+//The gap between each of the Kanban board columns.
+const GAP_BETWEEN_COLUMN_RECTANGLES = 14;
+//The additional gap at the left and right sides of the screen, as when you divide an odd number is produced so must
+//balance things out.
+const ADDITIONAL_GAP_BESIDE_COLUMN_RECTANGLES_LEFT_RIGHT_SCREEN = 1;
 
 export default class KanbanBoard{
 
@@ -13,33 +30,33 @@ export default class KanbanBoard{
         //Source: https://newdocs.phaser.io/docs/3.80.0/focus/Phaser.GameObjects.GameObjectFactory-text
         //Source: https://newdocs.phaser.io/docs/3.80.0/Phaser.Types.GameObjects.Text.TextStyle
         //Some code from ChatGPT:
-        //650 as this is the middle.
-        const titleText = this.add.text(650, TOP_TO_TITLE_GAP, 'Kanban Board', { fontSize: TITLE_TEXT_HEIGHT + 'px', fontFamily: 'Calibri', fill: '#fff' });
+        //650 as kanbanStation is the middle.
+        const titleText = kanbanStation.add.text(650, TOP_TO_TITLE_GAP, 'Kanban Board', { fontSize: TITLE_TEXT_HEIGHT + 'px', fontFamily: 'Calibri', fill: '#fff' });
 
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
         titleText.setOrigin(0.5, 0);
 
         // Navigation buttons (from Station.js)
-        this.createNavigationTabs();
+        kanbanStation.createNavigationTabs();
 
         // Create tomato paste image
-        //this.createTomatoPasteBottle();
+        //kanbanStation.createTomatoPasteBottle();
 
         // Create add task button
-        const addTaskButton = this.add.text(100, 200, 'Add Task', { fontSize: '24px', fill: '#fff', backgroundColor: '#17a2b8' })
+        const addTaskButton = kanbanStation.add.text(100, 200, 'Add Task', { fontSize: '24px', fill: '#fff', backgroundColor: '#17a2b8' })
             .setInteractive()
-            .on('pointerdown', () => this.addTask());
+            .on('pointerdown', () => kanbanStation.addTask());
 
         // Listen for kanban tasks updates
-        this.game.socket.on('kanbanTasksUpdate', (tasks) => {
-            this.tasks = tasks;
-            this.updateKanbanDisplay();
+        kanbanStation.game.socket.on('kanbanTasksUpdate', (tasks) => {
+            kanbanStation.tasks = tasks;
+            kanbanStation.updateKanbanDisplay();
         });
 
         // Get initial game state
-        this.game.socket.on('initialGameState', (gameState) => {
-            this.tasks = gameState.kanbanTasks;
-            this.updateKanbanDisplay();
+        kanbanStation.game.socket.on('initialGameState', (gameState) => {
+            kanbanStation.tasks = gameState.kanbanTasks;
+            kanbanStation.updateKanbanDisplay();
         });
         
         //Add the 6 rectangles or other objects to represent the columns on the Kanban board.
@@ -52,13 +69,13 @@ export default class KanbanBoard{
         console.log("Y_TOP_COLUMN_RECTANGLES = " + Y_TOP_COLUMN_RECTANGLES);
         
         //Some stuff from ChatGPT.
-        const Y_BOTTOM_COLUMN_RECTANGLES = this.scale.height - (COLUMN_RECTANGLES_TO_MENU_GAP + Y_MENU_START);
+        const Y_BOTTOM_COLUMN_RECTANGLES = kanbanStation.scale.height - (COLUMN_RECTANGLES_TO_MENU_GAP + Y_MENU_START);
         console.log("Y_BOTTOM_COLUMN_RECTANGLES = " + Y_BOTTOM_COLUMN_RECTANGLES);
         //The height of the column rectangles for the Kanban board.
         const COLUMN_RECTANGLE_HEIGHT = Y_BOTTOM_COLUMN_RECTANGLES - Y_TOP_COLUMN_RECTANGLES;
         console.log("COLUMN_RECTANGLE_HEIGHT = " + COLUMN_RECTANGLE_HEIGHT);
 
-        //This variable will be incremented as I draw each of the rectangles for the columns on the Kanban board.
+        //kanbanStation variable will be incremented as I draw each of the rectangles for the columns on the Kanban board.
         //Set it at the initial position for the first rectangle.
         let currentXColumnRectangleStartPos = GAP_BETWEEN_COLUMN_RECTANGLES + ADDITIONAL_GAP_BESIDE_COLUMN_RECTANGLES_LEFT_RIGHT_SCREEN;
 
@@ -70,42 +87,42 @@ export default class KanbanBoard{
         }
 
         console.log("Before drawing 1st column, currentXColumnRectangleStartPos = " + currentXColumnRectangleStartPos);
-        const orderColumnRectangle = this.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
+        const orderColumnRectangle = kanbanStation.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
             COLUMN_RECTANGLE_WIDTH, COLUMN_RECTANGLE_HEIGHT, 0x4fa632);
         //Set the origin point which is used to set the X and Y positions to the top left of the object.
         orderColumnRectangle.setOrigin(0, 0);
         increaseCurrentXColumnRectangleStartPos(); //Increase the starting X position before drawing the next column rectangle.
 
         console.log("Before drawing 2nd column (prep), currentXColumnRectangleStartPos = " + currentXColumnRectangleStartPos);
-        const prepColumnRectangle = this.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
+        const prepColumnRectangle = kanbanStation.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
             COLUMN_RECTANGLE_WIDTH, COLUMN_RECTANGLE_HEIGHT, 0x4fa632);
         //Set the origin point which is used to set the X and Y positions to the top left of the object.
         prepColumnRectangle.setOrigin(0, 0);
         increaseCurrentXColumnRectangleStartPos(); //Increase the starting X position before drawing the next column rectangle.
 
         console.log("Before drawing 3rd column (cook), currentXColumnRectangleStartPos = " + currentXColumnRectangleStartPos);
-        const cookColumnRectangle = this.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
+        const cookColumnRectangle = kanbanStation.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
             COLUMN_RECTANGLE_WIDTH, COLUMN_RECTANGLE_HEIGHT, 0x4fa632);
         //Set the origin point which is used to set the X and Y positions to the top left of the object.
         cookColumnRectangle.setOrigin(0, 0);
         increaseCurrentXColumnRectangleStartPos(); //Increase the starting X position before drawing the next column rectangle.
 
         console.log("Before drawing 4th column (review), currentXColumnRectangleStartPos = " + currentXColumnRectangleStartPos);
-        const reviewColumnRectangle = this.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
+        const reviewColumnRectangle = kanbanStation.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
             COLUMN_RECTANGLE_WIDTH, COLUMN_RECTANGLE_HEIGHT, 0x4fa632);
         //Set the origin point which is used to set the X and Y positions to the top left of the object.
         reviewColumnRectangle.setOrigin(0, 0);
         increaseCurrentXColumnRectangleStartPos(); //Increase the starting X position before drawing the next column rectangle.
 
         console.log("Before drawing 5th column (service), currentXColumnRectangleStartPos = " + currentXColumnRectangleStartPos);
-        const serviceColumnRectangle = this.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
+        const serviceColumnRectangle = kanbanStation.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
             COLUMN_RECTANGLE_WIDTH, COLUMN_RECTANGLE_HEIGHT, 0x4fa632);
         //Set the origin point which is used to set the X and Y positions to the top left of the object.
         serviceColumnRectangle.setOrigin(0, 0);
         increaseCurrentXColumnRectangleStartPos(); //Increase the starting X position before drawing the next column rectangle.
 
         console.log("Before drawing last (6th) column (completed), currentXColumnRectangleStartPos = " + currentXColumnRectangleStartPos);
-        const completedColumnRectangle = this.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
+        const completedColumnRectangle = kanbanStation.add.rectangle(currentXColumnRectangleStartPos, Y_TOP_COLUMN_RECTANGLES,
             COLUMN_RECTANGLE_WIDTH, COLUMN_RECTANGLE_HEIGHT, 0x4fa632);
         //Set the origin point which is used to set the X and Y positions to the top left of the object.
         completedColumnRectangle.setOrigin(0, 0);
@@ -117,12 +134,12 @@ export default class KanbanBoard{
 
         const Y_TOP_COLUMN_TITLES = TOP_TO_TITLE_GAP + TITLE_TEXT_HEIGHT + TITLE_TO_COLUMN_TITLES_HEIGHT
 
-        //This variable will be incremented as I draw each of the rectangles for the columns on the Kanban board.
+        //kanbanStation variable will be incremented as I draw each of the rectangles for the columns on the Kanban board.
         //Set it at the initial position for the first rectangle.
         let currentXColumnTitleStartPos = GAP_BETWEEN_COLUMN_RECTANGLES + ADDITIONAL_GAP_BESIDE_COLUMN_RECTANGLES_LEFT_RIGHT_SCREEN;
-        //Will also need to centre it, not in this variable but in the actual code to display the text on the screen.
+        //Will also need to centre it, not in kanbanStation variable but in the actual code to display the text on the screen.
         let currentXColumnTitleCenterPos = 0;
-        updateCurrentXColumnTitleCenterPos(); //Set this initially for the first time.
+        updateCurrentXColumnTitleCenterPos(); //Set kanbanStation initially for the first time.
 
         /**
          * Increment the position to start drawing each new column title in the Kanban board.
@@ -142,55 +159,55 @@ export default class KanbanBoard{
         }
 
         console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
-        const orderText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Orders', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
+        const orderText = kanbanStation.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Orders', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
             fontFamily: 'Calibri', fill: '#fff' });
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
         orderText.setOrigin(0.5, 0);
         //Update the two variables that store the X positions for drawing the text.
         increaseCurrentXColumnTitleStartPos();
 
         console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
-        const prepText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Preparation', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
+        const prepText = kanbanStation.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Preparation', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
             fontFamily: 'Calibri', fill: '#fff' });
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
         prepText.setOrigin(0.5, 0);
         //Update the two variables that store the X positions for drawing the text.
         increaseCurrentXColumnTitleStartPos();
 
         console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
-        const cookText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Cooking and Cutting', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
+        const cookText = kanbanStation.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Cooking and Cutting', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
             fontFamily: 'Calibri', fill: '#fff' });
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
         cookText.setOrigin(0.5, 0);
         //Update the two variables that store the X positions for drawing the text.
         increaseCurrentXColumnTitleStartPos();
 
         console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
-        const reviewText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Reviewing', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
+        const reviewText = kanbanStation.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Reviewing', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
             fontFamily: 'Calibri', fill: '#fff' });
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
         reviewText.setOrigin(0.5, 0);
         //Update the two variables that store the X positions for drawing the text.
         increaseCurrentXColumnTitleStartPos();
 
         console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
-        const serviceText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Service', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
+        const serviceText = kanbanStation.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Service', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
             fontFamily: 'Calibri', fill: '#fff' });
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
         serviceText.setOrigin(0.5, 0);
         //Update the two variables that store the X positions for drawing the text.
         increaseCurrentXColumnTitleStartPos();
 
         console.log("currentXColumnTitleCenterPos = " + currentXColumnTitleCenterPos);
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
-        const completedText = this.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Completed Pizzas', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
+        const completedText = kanbanStation.add.text(currentXColumnTitleCenterPos, Y_TOP_COLUMN_TITLES, 'Completed Pizzas', { fontSize: COLUMN_TITLES_TEXT_HEIGHT + 'px',
             fontFamily: 'Calibri', fill: '#fff' });
-        //Set the X origin to the center of the text. This makes it easier to centre it. Keep the Y origin to the top.
+        //Set the X origin to the center of the text. kanbanStation makes it easier to centre it. Keep the Y origin to the top.
         completedText.setOrigin(0.5, 0);
         //Update the two variables that store the X positions for drawing the text.
         increaseCurrentXColumnTitleStartPos();
@@ -203,7 +220,7 @@ export default class KanbanBoard{
 
     createTestLabels(){
         //Create several test labels on the Kanban board automatically for testing purposes.
-        //This code should be removed later on.
+        //kanbanStation code should be removed later on.
     }
 
 }

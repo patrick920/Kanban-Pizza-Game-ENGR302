30e73969f3b89838728916061b0730a8f4861610
millspatr1 <millspatr1@ecs.vuw.ac.nz>
2024-11-01 11:33:54 +1300

    Created two new files which will be used for the Kanban board and the labels.

---
+++ server/public/stations/kanban/KanbanBoard.js
@@ -0,0 +1,7 @@
+/*
+ * This file will hold all the display code for the Kanban board, to keep KanbanStation.js cleaner and not too long.
+ */
+
+//TODO: Have a list for each of the stations with the labels currently in there. Have a hardcoded list for each
+//one as it will be simpler.
+

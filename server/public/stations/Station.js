4a49e1f3a7425c9442e44cff0981a2a8bd4e65ff
millspatr1 <millspatr1@ecs.vuw.ac.nz>
2024-11-06 01:31:59 +1300

    I removed the buttons to the stations except the button to the Order Station and the Kanban Station.

--- server/public/stations/Station.js
+++ server/public/stations/Station.js
@@ -7,13 +7,13 @@ export default class Station extends Phaser.Scene {
             .setInteractive()
             .on('pointerdown', () => this.scene.start('OrderStation'));
 
-        const prepareButton = this.add.text(300, buttonY, 'Prepare', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#ffc107' })
-            .setInteractive()
-            .on('pointerdown', () => this.scene.start('PrepareStation'));
+        // const prepareButton = this.add.text(300, buttonY, 'Prepare', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#ffc107' })
+        //     .setInteractive()
+        //     .on('pointerdown', () => this.scene.start('PrepareStation'));
 
-        const cookButton = this.add.text(500, buttonY, 'Cook', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#28a745' })
-            .setInteractive()
-            .on('pointerdown', () => this.scene.start('CookStation'));
+        // const cookButton = this.add.text(500, buttonY, 'Cook', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#28a745' })
+        //     .setInteractive()
+        //     .on('pointerdown', () => this.scene.start('CookStation'));
         
         const kanbanButton = this.add.text(700, buttonY, 'Kanban Board', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#28a745' })
             .setInteractive()
@@ -26,8 +26,8 @@ export default class Station extends Phaser.Scene {
                 this.scene.start('KanbanStation');
             });        
 
-        const reviewButton = this.add.text(900, buttonY, 'Review', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#a7288a' })
-            .setInteractive()
-            .on('pointerdown', () => this.scene.start('ReviewStation'));
+        // const reviewButton = this.add.text(900, buttonY, 'Review', { fontSize: '20px', fill: '#fff', fontFamily: 'Calibri', backgroundColor: '#a7288a' })
+        //     .setInteractive()
+        //     .on('pointerdown', () => this.scene.start('ReviewStation'));
     }
 }
